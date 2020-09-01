fab61ee0a2bbff779589186d715dd6953c879d27
AMR.VIJAYAKUMAR <ajeyamadhavarao@gmail.com>
2020-09-01 21:39:22 +0530

    Updates

---
+++ precache-manifest.370c45815cacaf3b01f160f16fa137d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c815369c85080e51d54dc9d937c2a602",
+    "url": "/laugh-inator/index.html"
+  },
+  {
+    "revision": "4069c66ff95161d8ba5c",
+    "url": "/laugh-inator/static/css/main.ff31e9f5.chunk.css"
+  },
+  {
+    "revision": "2d90498436d14edd9c5d",
+    "url": "/laugh-inator/static/js/2.c66e3625.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/laugh-inator/static/js/2.c66e3625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4069c66ff95161d8ba5c",
+    "url": "/laugh-inator/static/js/main.914bb687.chunk.js"
+  },
+  {
+    "revision": "341e38e795aec63bb209e8b6d69805eb",
+    "url": "/laugh-inator/static/js/main.914bb687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493a4caea48c0f16ca14",
+    "url": "/laugh-inator/static/js/runtime-main.c640453a.js"
+  }
+]);
